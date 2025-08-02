b5cf6797ce161c5e9c649ce090b80af2a67d2ee5
DESKTOP-8FEL777\user <chlhyn1990@gmail.com>
2025-08-03 02:33:47 +0900

    응답코드 추가

--- src/main/java/com/SCSystem/dto/ApiResult.java
+++ src/main/java/com/SCSystem/dto/ApiResult.java
@@ -3,7 +3,13 @@ package com.SCSystem.dto;
 
 public class ApiResult {
 	final static public String SUCCESS = "0000";
+	final static public String SERVER_ERROR = "1001";
+	final static public String REQUIRED_VALUE_NULL = "1002";
+	final static public String ETC = "8888";
 	
 	final static public String SUCCESS_MSG = "성공 코드";
+	final static public String SERVER_ERROR_MSG = "서버 측 오류";
+	final static public String REQUIRED_VALUE_NULL_MSG = "필수 값 누락";
+	final static public String ETC_MSG = "기타코드";
 	
 }
