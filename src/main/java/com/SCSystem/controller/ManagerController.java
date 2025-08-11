095fb0cc326a3a2541a45aa237fb082e7483cb51
chlhyn1990 <chlhyn1990@gmail.com>
2025-08-11 17:14:50 +0900

    매니저 관리 추가

--- src/main/java/com/SCSystem/controller/ManagerController.java
+++ src/main/java/com/SCSystem/controller/ManagerController.java
@@ -1,9 +1,12 @@
 package com.SCSystem.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.SCSystem.dto.ApiResult;
+import com.SCSystem.dto.Company;
 import com.SCSystem.dto.Manager;
 import com.SCSystem.service.ManagerService;
 
@@ -22,23 +26,60 @@ import lombok.extern.slf4j.Slf4j;
 public class ManagerController {
 
 	@Autowired
-	ManagerService managerService;
+	ManagerService service;
 	
 	@PostMapping("/login")
 	@ResponseBody
-	public ResponseEntity<Manager> getManager(@RequestBody  Manager manager){
-		Manager loginManager = managerService.getManager(manager);
+	public ResponseEntity<Manager> login(@RequestBody  Manager dto){
+		Manager loginManager = service.login(dto);
         return new ResponseEntity<>(
         		loginManager,
                 HttpStatus.OK
         		);
 	}
 	
+	@PostMapping({"/list", "/list/{search}"})
+	@ResponseBody
+	public ResponseEntity<List<Manager>> getCompanyList(@PathVariable(required = false) String search){
+		List<Manager> dtos = service.getList(search);
+        return new ResponseEntity<>(
+        		dtos,
+                HttpStatus.OK
+        		);
+	}
+	
+	@PostMapping("/{idx}")
+	@ResponseBody
+	public ResponseEntity<Manager> getCompany(@PathVariable int idx){
+		Manager manager = service.get(idx);
+        return new ResponseEntity<>(
+        		manager,
+                HttpStatus.OK
+        		);
+	}
+	
+	@PostMapping("/delete/{idx}")
+	@ResponseBody
+	public ResponseEntity<ApiResult> deleteCompany(@PathVariable  int idx){
+		ApiResult apiResult = new ApiResult();
+		if(service.delete(idx) == 1){
+			apiResult.setCode(ApiResult.SUCCESS);
+			apiResult.setMsg(ApiResult.SUCCESS_MSG);
+		}else {
+			apiResult.setCode(ApiResult.COMPANY_DELETE_FAIL);
+			apiResult.setMsg(ApiResult.COMPANY_DELETE_FAIL_MSG);
+		}
+		return new ResponseEntity<>(
+				apiResult,
+                HttpStatus.OK
+				);
+	}
+	
 	@PostMapping("/insert")
 	@ResponseBody
-	public ResponseEntity<ApiResult> insertManager(@RequestBody  Manager manager){
+	public ResponseEntity<ApiResult> insert(@RequestBody  Manager dto){
 		ApiResult apiResult = new ApiResult();
-		if(managerService.insertManager(manager) == 1){
+		if(service.insert(dto) == 1){
 			apiResult.setCode(ApiResult.SUCCESS);
 			apiResult.setMsg(ApiResult.SUCCESS_MSG);
 		}else {
@@ -50,8 +91,23 @@ public class ManagerController {
                 HttpStatus.OK
 				);
 	}
-
 	
+	@PostMapping("/update")
+	@ResponseBody
+	public ResponseEntity<ApiResult> update(@RequestBody  Manager dto){
+		ApiResult apiResult = new ApiResult();
+		if(service.update(dto) == 1){
+			apiResult.setCode(ApiResult.SUCCESS);
+			apiResult.setMsg(ApiResult.SUCCESS_MSG);
+		}else {
+			apiResult.setCode(ApiResult.COMPANY_UPDATE_FAIL);
+			apiResult.setMsg(ApiResult.COMPANY_UPDATE_FAIL_MSG);
+		}
+		return new ResponseEntity<>(
+				apiResult,
+                HttpStatus.OK
+				);
+	}
 	
 }
 
--- src/main/java/com/SCSystem/mapper/ManagerMapper.java
+++ src/main/java/com/SCSystem/mapper/ManagerMapper.java
@@ -1,14 +1,27 @@
 package com.SCSystem.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import com.SCSystem.dto.Manager;
 
 @Mapper
 public interface ManagerMapper {
-
-	Manager getManager(Manager manager);
 	
-	int insertManager(Manager manager);
+	List<Manager> getList();
+	
+	List<Manager> getSearchList(String search);
+	
+	Manager get(int idx);
+
+	Manager login(Manager manager);
+	
+	int insert(Manager manager);
+	
+	int update(Manager manager);
+	
+	int delete(int idx);
 
 }
 
