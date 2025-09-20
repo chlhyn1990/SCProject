99ddaaa15d60a3bc709485026d2debc29d2666cb
chlhyn1990 <chlhyn1990@gmail.com>
2025-09-21 03:20:25 +0900

    check_mst (점검표 마스터) 추가

---
+++ src/main/java/com/SCSystem/dto/CheckMst.java
@@ -0,0 +1,24 @@
+package com.SCSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CheckMst {
+	private int idx;
+	private int company_idx;
+	private int manager_idx;
+	private int charger_station_idx;
+	private int distribution_idx;
+	
+	private String check_dt;
+	private String temperature;
+
+	private String modify_dt;
+	private String create_dt;
+}
