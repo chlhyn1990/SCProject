065e7fbef1f117f99fbed897f5ecb3ce1cd36ffb
chlhyn1990 <chlhyn1990@gmail.com>
2025-10-13 02:46:24 +0900

    엑셀파일추가

--- src/main/java/com/SCSystem/service/CheckService.java
+++ src/main/java/com/SCSystem/service/CheckService.java
@@ -176,7 +176,7 @@ public class CheckService {
 	    chargerStation.setDistributionList(distributionService.getListByStation(check.getCheckMst().getCharger_station_idx()));
 	    
 	    try {
-	        templateFile = new File("C:\\dev\\project\\sheet1.xlsx");
+	        templateFile = new File("sheet1.xlsx");
 	        if (templateFile.exists() && templateFile.isFile()) {
 	            templateStream = new FileInputStream(templateFile);
 	        } 
